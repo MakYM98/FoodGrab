964ef02a17195cdfb6b04b4ca0b6c126d36235ab
YeongMeng <makyeongmeng@gmail.com>
2023-02-06 21:46:33 +0800

    Fridge Automation

--- src/fridge/fridge.js
+++ src/fridge/fridge.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 import Table from 'react-bootstrap/Table';
+import axios from 'axios'
 
 delete L.Icon.Default.prototype._getIconUrl;
 
@@ -12,54 +13,68 @@ L.Icon.Default.mergeOptions({
     shadowUrl: require('leaflet/dist/images/marker-shadow.png')
 });
 
-var locationData = require('../data/fridge_location.json')['locations']
-
 const singaporePostion = [1.3521, 103.8198]
 
 function CommunityFridge() {
-    return (
-      <div style={{display:'flex', justifyContent:'center', marginTop:'3%'}}>
-        <div style={{width:'80%'}}>
-          <MapContainer center={singaporePostion} zoom={12} scrollWheelZoom={false} style={{ height: '60vh', width: '60wh!important' }}>
-            <TileLayer
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            {
-              locationData.map(location => 
-                  <Marker position={[location['latitude'], location['longitude']]}>
-                    <Popup>
-                      {location['street']}
-                    </Popup>
-                  </Marker>
-              )
-            }
-          </MapContainer>
+  const [fridgeLocations, setFridgeLocations] = useState([])
 
-          <div style={{marginTop:'5%', marginBottom:'5%'}}>
-            <h3 style={{textAlign:'left'}}>List of Locations</h3>
-            <Table striped bordered hover>
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>Street Name</th>
-                </tr>
-              </thead>
-              <tbody>
-                {
-                  locationData.map(location =>
-                    <tr>
-                      <td>{location['no.']}</td>
-                        <td style={{textAlign:'left'}}>{location['street']}</td>
-                    </tr> 
-                  )
-                }
-              </tbody>
-            </Table>
-          </div>
+  const fetchFridges = async () => {
+    var queryString = "http://127.0.0.1:8000/api/fridges"
+    axios
+        .get(queryString)
+        .then(response => {
+            setFridgeLocations(response.data)
+        })
+        .catch(error => console.error(`Error retrieving Login Info: ${error}`))
+  }
+
+  useEffect(()=>{
+    fetchFridges()
+  },[])
+
+  return (
+    <div style={{display:'flex', justifyContent:'center', marginTop:'3%'}}>
+      <div style={{width:'80%'}}>
+        <MapContainer center={singaporePostion} zoom={12} scrollWheelZoom={false} style={{ height: '60vh', width: '60wh!important' }}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {
+            fridgeLocations.map(location => 
+                <Marker position={[location['latitude'], location['longitude']]}>
+                  <Popup>
+                    {location['street']}
+                  </Popup>
+                </Marker>
+            )
+          }
+        </MapContainer>
+
+        <div style={{marginTop:'5%', marginBottom:'5%'}}>
+          <h3 style={{textAlign:'left'}}>List of Locations</h3>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Street Name</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                fridgeLocations.map(location =>
+                  <tr>
+                    <td>{location['fridge_id']}</td>
+                      <td style={{textAlign:'left'}}>{location['street']}</td>
+                  </tr> 
+                )
+              }
+            </tbody>
+          </Table>
         </div>
       </div>
-    )
+    </div>
+  )
   }
 
   export default CommunityFridge
